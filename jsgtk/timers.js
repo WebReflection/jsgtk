25000f813b9da18d0a015691a338e3e4f17adb10
webreflection <andrea.giammarchi@gmail.com>
2015-12-07 09:40:58 +0000

    using shortcuts for timers

--- jsgtk/timers.js
+++ jsgtk/timers.js
@@ -2,25 +2,19 @@
 
   const
     Mainloop = imports.mainloop,
-    global = imports.jsgtk.System.global
+    global = imports.jsgtk.System.global,
+    clear = () => (id) => Mainloop.source_remove(id),
+    set = (repeat) =>
+      (fn, ms, ...args) =>
+        Mainloop.timeout_add((ms * 1) || 0, () => {
+          fn.apply(null, args);
+          return repeat;
+        })
   ;
 
   global.clearInterval  = (exports.clearInterval = clear());
   global.clearTimeout   = (exports.clearTimeout = clear());
   global.setInterval    = (exports.setInterval = set(true));
   global.setTimeout     = (exports.setTimeout = set(false));
-
-  function clear() {
-    return (id) => Mainloop.source_remove(id);
-  }
-
-  function set(repeat) {
-    return function (fn, ms, ...args) {
-      return Mainloop.timeout_add((ms * 1) || 0, () => {
-        fn.apply(null, args);
-        return repeat;
-      });
-    };
-  }
 
 }(this));
