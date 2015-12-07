ce17dbb06af7a9b39f8c6c2b90fbfbb294c94deb
webreflection <andrea.giammarchi@gmail.com>
2015-12-07 09:37:52 +0000

    faster timers

--- jsgtk/timers.js
+++ jsgtk/timers.js
@@ -14,12 +14,8 @@
     return (id) => Mainloop.source_remove(id);
   }
 
-  function set(repeat) {    // spec compliant
-    return function (fn, ms /*, ...args*/) {
-      const args = [];
-      for (let i = 2; i < arguments.length; i++) {
-        args[i - 2] = arguments[i];
-      }
+  function set(repeat) {
+    return function (fn, ms, ...args) {
       return Mainloop.timeout_add((ms * 1) || 0, () => {
         fn.apply(null, args);
         return repeat;
