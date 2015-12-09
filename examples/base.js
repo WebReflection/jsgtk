804a3d7ab79f8f29dff470d404933c23495b42f6
webreflection <andrea.giammarchi@gmail.com>
2015-12-09 17:38:45 +0000

    added automatic environment for jsgtk

---
+++ examples/base.js
@@ -0,0 +1,8 @@
+// if you have installed jsgtk globally via npm
+// you can simply run: jsgtk base.js
+console.info('Hello JSGTK!');
+console.log([
+  __filename,
+  __dirname,
+  process.cwd()
+].join('\n'));
