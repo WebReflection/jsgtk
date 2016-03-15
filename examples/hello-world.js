0ad7e5ae0fdd7b5eb4bbb98938bfb8f55b547bcc
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2016-03-15 16:43:47 +0000

    better hello-world.js example

--- examples/hello-world.js
+++ examples/hello-world.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env jsgtk
 
 let
+  argv = process.argv.slice(2),
   Gtk = require('Gtk'),
   win
 ;
@@ -9,8 +10,21 @@ Gtk.init(null);
 
 win = new Gtk.Window({
   title: 'jsgtk',
+  type : Gtk.WindowType.TOPLEVEL,
   windowPosition: Gtk.WindowPosition.CENTER
 });
+
+// Setting up optional Dark theme (gotta love it!)
+// ./browser.js google.com --dark
+if (argv.some(info => info === '--dark')) {
+  let gtkSettings = Gtk.Settings.getDefault();
+  gtkSettings.gtkApplicationPreferDarkTheme = true;
+  gtkSettings.gtkThemeName = 'Adwaita';
+} else if(argv.some(info => info === '--light')) {
+  let gtkSettings = Gtk.Settings.getDefault();
+  gtkSettings.gtkApplicationPreferDarkTheme = false;
+  gtkSettings.gtkThemeName = 'Adwaita';
+}
 
 win.connect('show', Gtk.main);
 win.connect('destroy', Gtk.mainQuit);
