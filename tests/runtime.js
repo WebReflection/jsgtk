9f5c5043b0d0365806374a2661fc825eb1cd3148
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2016-03-22 17:56:55 +0000

    fixed npm install local issue

--- tests/runtime.js
+++ tests/runtime.js
@@ -1,15 +1,17 @@
+/*
 imports.gi.GIRepository.Repository.get_default().get_loaded_namespaces().forEach((name) => {
   var t = Date.now();
   require(name);
   t = Date.now() - t;
   print(name + ': ' + (t / 1000).toFixed(3));
 });
+//*/
 
-/*
+//*
 const
   Gio = require('Gio'),
   current = Gio.File.newForPath(__filename)
 ;
 
 print(current.getPath());
-*/
+//*/
