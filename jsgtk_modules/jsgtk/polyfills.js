35b35ab9df5915ef8e23ec25ee06bfa86e117994
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2016-03-23 10:50:51 +0000

    better polyfills

--- jsgtk_modules/jsgtk/polyfills.js
+++ jsgtk_modules/jsgtk/polyfills.js
@@ -1,34 +1,67 @@
 /*! © Andrea Giammarchi @WebReflection */
 
-/* jshint esversion: 6, strict: true, node: true */
+/* jshint esversion: 6, strict: implied */
 
-// partially populated at runtime on bootstrap
-
-(function (exports, Object) {'use strict';
-
-  if (!('setPrototypeOf' in Object)) {
-    Object.defineProperty(
-      Object,
-      'setPrototypeOf',
-      {
-        enumerable: true,
-        value: ((set) => function setPrototypeOf(target, proto) {
-          set.call(target, proto);
-          return target;
-        })(Object.getOwnPropertyDescriptor(Object.prototype, '__proto__').set)
+// inline polyfills for Object
+[
+  function setPrototypeOf() {
+    var set = Object.getOwnPropertyDescriptor(
+      Object.prototype,
+      '__proto__'
+    ).set;
+    return {
+      configurable: true,
+      writable: true,
+      value: function setPrototypeOf(target, proto) {
+        set.call(target, proto);
+        return target;
       }
-    );
-  }
-
-  if (!('getOwnPropertySymbols' in Object)) {
-    Object.defineProperty(
-      Object,
-      'getOwnPropertySymbols',
-      {
-        enumerable: true,
-        value: function getOwnPropertySymbols(){ return []; }
+    };
+  },
+  function getOwnPropertyDescriptors() {
+    var
+      defineProperty = Object.defineProperty,
+      gOPD = Object.getOwnPropertyDescriptor,
+      gOPN = Object.getOwnPropertyNames,
+      gOPS = Object.getOwnPropertySymbols,
+      ownKeys = gOPS ?
+        function (target) {
+          return gOPN(target).concat(gOPS(target));
+        } :
+        gOPN
+    ;
+    return {
+      configurable: true,
+      writable: true,
+      value: function getOwnPropertyDescriptors(target) {
+        return ownKeys(target).reduce(
+          function (descriptors, name) {
+            return defineProperty(
+              descriptors,
+              name,
+              gOPD(target, name)
+            );
+          },
+          {}
+        );
       }
-    );
+    };
+  },
+  function getOwnPropertySymbols() {
+    return {
+      configurable: true,
+      writable: true,
+      value: function getOwnPropertySymbols(target) {
+        return [];
+      }
+    };
   }
-
-}(this, Object));
+].forEach(
+  function poly(fill) {
+    if (fill.name in this) return;
+    var descriptor = fill(this);
+    if (descriptor)
+      Object.defineProperty(this, fill.name, descriptor);
+  },
+  Object
+);
