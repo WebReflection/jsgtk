e0c3bde1ddd8cef04b9eb6f8b896cf33d0e0bfe4
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2016-05-05 01:58:51 +0100

    Updating to 0.9.20

--- jsgtk_modules/jsgtk/constants.js
+++ jsgtk_modules/jsgtk/constants.js
@@ -1,1 +1,1 @@
-/*jshint esversion:6,strict:true,node:true*/(function (e) {'use strict';e.JSGTK=true;e.VERSION='0.9.19';}(this));
+/*jshint esversion:6,strict:true,node:true*/(function (e) {'use strict';e.JSGTK=true;e.VERSION='0.9.20';}(this));
