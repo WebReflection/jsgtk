761e3c5362b36c842505aa6f4ad1336c83cb2c10
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2017-04-14 13:40:35 +0100

    Updating to 0.11.4

--- jsgtk_modules/jsgtk/constants.js
+++ jsgtk_modules/jsgtk/constants.js
@@ -1,1 +1,1 @@
-/*jshint esversion:6,strict:true,node:true*/(function (e) {'use strict';e.JSGTK=true;e.VERSION='0.11.3';}(this));
+/*jshint esversion:6,strict:true,node:true*/(function (e) {'use strict';e.JSGTK=true;e.VERSION='0.11.4';}(this));
